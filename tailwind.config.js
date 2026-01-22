6de20fcab34be14cebcec33b8706ae27e1da03a0
lhn-gif <lhn@ozys.net>
2026-01-22 16:07:09 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// postcss.config.js
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
